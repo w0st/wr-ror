cbae0b397d2c7733dd915a2043f13010f23b25dc
Wojciech Stępniak <wojtek@niom.pl>
2015-12-05 01:38:09 +0100

    Update assignment between subjects and teachers/students
    Student can add any subject.
    Teacher can add unassigned subjects.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,6 +2,7 @@ class TeachersController < ApplicationController
   expose(:teachers)
   expose(:teacher, attributes: :teacher_params)
   expose(:teacher_subject_items) { teacher.subject_items }
+  expose(:subject_items) {SubjectItem.not_assigned_or_assigned_to_teacher(teacher)}
 
   def create
     if teacher.save
