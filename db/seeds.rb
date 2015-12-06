2334cefedccbecb057496918a5dbf356f29af26d
Wojciech Stępniak <wojtek@niom.pl>
2015-12-06 09:13:39 +0100

    Update seeds
    Seeds refer to the guidelines

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
+SUBJECT_TITLES = %w(Mathematics Software\c Engineering Statistics Philosophy Management)
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
-30.times do
+
+3.times do
   Teacher.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -11,14 +13,14 @@ User.create!(email: 'admin@admin.com',password: 'adminadmin')
 end
 
 teachers = Teacher.all
-20.times do
+5.times do
   SubjectItem.create!(
-    title: Faker::Lorem.sentence,
+    title: SUBJECT_TITLES.pop,
     teacher: teachers.sample
   )
 end
 
-40.times do
+25.times do
   Student.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name
